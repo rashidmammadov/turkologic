5b2850a6a0a3af038b34db0889378b79b5fc859f
Rashid Mammadov <reshid@live.com>
2019-10-15 18:05:43 +0300

    Updated web project.

--- web/src/app/heat-map/heat-map.component.ts
+++ web/src/app/heat-map/heat-map.component.ts
@@ -9,104 +9,104 @@ let svg, legend, width, height, tooltip, color, margin = 8,
   styleUrls: ['./heat-map.component.css']
 })
 export class HeatMapComponent implements OnChanges {
-  // @ts-ignore
-  @ViewChild('chart') chartContainer: ElementRef;
-  @Input() data: any;
-
-  constructor() { }
-
-  private prepare() {
-    d3.select('svg.heat-map').remove();
-    const element = this.chartContainer.nativeElement;
-
-    width = element.clientWidth;
-    height = width / 2.5;
-    svg = d3.select(element).append('svg').classed('heat-map', true)
-      .attr('width', width).attr('height', height)
-      .append('g').attr('transform', 'translate(' + margin * 2 + ',' + margin * 2 + ')');
-
-    legend = svg.append('g').attr('transform', 'translate(' + (width - margin * 7) + ',' + margin * 2 + ')')
-      .selectAll('.legend').data(legendData);
-
     // @ts-ignore
-    color = d3.scaleLinear().domain([0, 1]).range(['#ffd300', '#586949']);
+    @ViewChild('chart') chartContainer: ElementRef;
+    @Input() data: any;
 
-    tooltip = d3.select(element).append('div')
-      .attr('class', 'directive-tooltip')
-      .style('display', 'none');
-  }
+    constructor() { }
 
-  private draw(data) {
-      let keys = d3.map(data,(d: any) => { return d.fromLexeme + ':' + d.fromLanguage; }).keys();
+    private prepare() {
+        d3.select('svg.heat-map').remove();
+        const element = this.chartContainer.nativeElement;
 
-      const x = d3.scaleBand().range([ 0, width - margin * 6 ]).domain(keys).padding(0.05);
+        width = element.clientWidth;
+        height = width / 2.5;
+        svg = d3.select(element).append('svg').classed('heat-map', true)
+            .attr('width', width).attr('height', height)
+            .append('g').attr('transform', 'translate(' + margin * 2 + ',' + margin * 2 + ')');
 
-      svg.append('g').classed('axisX', true)
-        .call(d3.axisBottom(x).tickSize(0))
-        .selectAll('text').text(d => d.split(':')[0]);
+        legend = svg.append('g').attr('transform', 'translate(' + (width - margin * 7) + ',' + margin * 2 + ')')
+            .selectAll('.legend').data(legendData);
 
-      const y = d3.scaleBand().range([ 0, height ]).domain(keys).padding(0.05);
+        // @ts-ignore
+        color = d3.scaleLinear().domain([0, 1]).range(['#ffd300', '#586949']);
 
-      svg.append('g').classed('axisY', true)
-        .call(d3.axisLeft(y).tickSize(0))
-        .selectAll('text').text(d => d.split(':')[0]);
+        tooltip = d3.select(element).append('div')
+            .attr('class', 'directive-tooltip')
+            .style('display', 'none');
+    }
 
-      legend.enter().append('rect').classed('legend', true)
-        .attr('y', (d, i) => {return (height / (legendData.length * 2)) * i})
-        .attr('width', 16)
-        .attr('height', height / (legendData.length * 2))
-        .attr('fill', (d) => color(d));
+    private draw(data) {
+        let keys = d3.map(data,(d: any) => { return d.fromLexeme + ':' + d.fromLanguage; }).keys();
 
-      legend.enter().append('text')
-        .style('font-size', '10px')
-        .attr('x', margin * 2 + 4)
-        .attr('y', (d, i) => {return (height / (legendData.length * 2)) * i + height / (legendData.length * 4)})
-        .text((d) => { return d * 100 + '%'; });
+        const x = d3.scaleBand().range([ 0, width - margin * 6 ]).domain(keys).padding(0.05);
 
-      const rectX = d3.scaleBand().range([ 0, width - margin * 10 ]).domain(keys).padding(0.05);
-      const rectY = d3.scaleBand().range([ 0, height - margin * 4 ]).domain(keys).padding(0.05);
-      const rects = svg.append('g').attr('transform', 'translate(' + margin * 2 + ',' + margin * 2 + ')')
-        .classed('rects', true).selectAll().data(data);
+        svg.append('g').classed('axisX', true)
+            .call(d3.axisBottom(x).tickSize(0))
+            .selectAll('text').text(d => d.split(':')[0]);
 
-      rects.exit().remove();
-      rects.enter().append('rect')
-        .attr('x', (d) => { return rectX(d.fromLexeme + ':' + d.fromLanguage) })
-        .attr('y', (d) => { return rectY(d.toLexeme + ':' + d.toLanguage) })
-        .attr('rx', 4)
-        .attr('ry', 4)
-        .attr('width', rectX.bandwidth())
-        .attr('height', rectY.bandwidth())
-        .style('fill', (d) => { return color(d.ratio); })
-        .on('mouseenter', this.showTooltip)
-        .on('mousemove', this.showTooltip)
-        .on('mouseleave', this.hideTooltip);
-  }
+        const y = d3.scaleBand().range([ 0, height ]).domain(keys).padding(0.05);
 
-  private showTooltip(d) {
-    let event = d3.event;
-    tooltip.style('left', event.offsetX + 'px');
-    tooltip.style('top', event.offsetY - margin + 'px');
+        svg.append('g').classed('axisY', true)
+            .call(d3.axisLeft(y).tickSize(0))
+            .selectAll('text').text(d => d.split(':')[0]);
 
-    tooltip.style('display', 'inline-block');
-    tooltip.html('<b>' + d.fromLexeme + '</b> (' + d.fromPronunciation + ') ' + d.fromLanguage + '<br/>' +
-      '<b>' + d.toLexeme + '</b> (' + d.toPronunciation + ') ' + d.toLanguage + '<br/>' +
-      'İlişki Yüzdesi: ' +  d.ratio * 100 + '%');
-  }
+        legend.enter().append('rect').classed('legend', true)
+            .attr('y', (d, i) => {return (height / (legendData.length * 2)) * i})
+            .attr('width', 16)
+            .attr('height', height / (legendData.length * 2))
+            .attr('fill', (d) => color(d));
 
-  private hideTooltip() {
-    tooltip.style('display', 'none');
-  }
+        legend.enter().append('text')
+            .style('font-size', '10px')
+            .attr('x', margin * 2 + 4)
+            .attr('y', (d, i) => {return (height / (legendData.length * 2)) * i + height / (legendData.length * 4)})
+            .text((d) => { return d * 100 + '%'; });
 
-  @HostListener('window:resize', ['$event'])
-  onResize() {
-    this.prepare();
-    this.draw(this.data);
-  }
+        const rectX = d3.scaleBand().range([ 0, width - margin * 10 ]).domain(keys).padding(0.05);
+        const rectY = d3.scaleBand().range([ 0, height - margin * 4 ]).domain(keys).padding(0.05);
+        const rects = svg.append('g').attr('transform', 'translate(' + margin * 2 + ',' + margin * 2 + ')')
+            .classed('rects', true).selectAll().data(data);
 
-  ngOnChanges() {
-    if (!this.data) { return; }
-    this.prepare();
-    this.draw(this.data);
-  }
+        rects.exit().remove();
+        rects.enter().append('rect')
+            .attr('x', (d) => { return rectX(d.fromLexeme + ':' + d.fromLanguage) })
+            .attr('y', (d) => { return rectY(d.toLexeme + ':' + d.toLanguage) })
+            .attr('rx', 4)
+            .attr('ry', 4)
+            .attr('width', rectX.bandwidth())
+            .attr('height', rectY.bandwidth())
+            .style('fill', (d) => { return color(d.ratio); })
+            .on('mouseenter', this.showTooltip)
+            .on('mousemove', this.showTooltip)
+            .on('mouseleave', this.hideTooltip);
+    }
+
+    private showTooltip(d) {
+        let event = d3.event;
+        tooltip.style('left', event.offsetX + 'px');
+        tooltip.style('top', event.offsetY - margin + 'px');
+
+        tooltip.style('display', 'inline-block');
+        tooltip.html('<b>' + d.fromLexeme + '</b> (' + d.fromPronunciation + ') ' + d.fromLanguage + '<br/>' +
+            '<b>' + d.toLexeme + '</b> (' + d.toPronunciation + ') ' + d.toLanguage + '<br/>' +
+            'İlişki Yüzdesi: ' +  d.ratio * 100 + '%');
+    }
+
+    private hideTooltip() {
+        tooltip.style('display', 'none');
+    }
+
+    @HostListener('window:resize', ['$event'])
+    onResize() {
+        this.prepare();
+        this.draw(this.data);
+    }
+
+    ngOnChanges() {
+        if (!this.data) { return; }
+        this.prepare();
+        this.draw(this.data);
+    }
 
 }
